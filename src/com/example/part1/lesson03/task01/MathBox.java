3b7667499f372eca59fc3a7dc1528347e1a6c102
Dayanova <fayagul.galieva@mail.ru>
2020-05-11 22:02:33 +0300

    поправила наименование

--- src/com/example/part1/lesson03/task01/MathBox.java
+++ src/com/example/part1/lesson03/task01/MathBox.java
@@ -3,9 +3,8 @@ package com.example.part1.lesson03.task01;
 import com.example.part1.lesson03.task02.ObjectBox;
 import com.example.person.Person;
 
+import java.awt.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Objects;
 /**
  * Класс MathBox с колекцией list
@@ -13,31 +12,28 @@ import java.util.Objects;
  * @ version 1.0
  */
 public class MathBox extends ObjectBox<Number> {
-    private ArrayList<Number> list= new ArrayList<>();
-
     /**
      * Конструктор - создание нового объекта
      */
     public MathBox(Number[] numbers) {
-
+        super(numbers);
         for (int i = 0; i < numbers.length; i++) {
-            int count = Collections.frequency(this.list, numbers[i]);
-
-            if (count > 1) {
+            if (this.sList.contains(numbers[i])) {
                 throw new ArithmeticException("duplication of values in an array");
             }
-            this.list.add(numbers[i]);
+            this.sList.add(numbers[i]);
         }
     }
-
 
     /**
      * метод расчитывающий сумму элементов коллекции
      */
     public int summator () {
         int s = 0;
-        for (int i = 0; i < this.list.size(); i++){
-            s+=(int) this.list.get(i);
+        ArrayList<Number> ms= new ArrayList<Number>();
+         ms.addAll(this.sList);
+        for (int i = 0; i < ms.size(); i++){
+            s+=(int) ms.get(i);
         }
         return s;
     }
@@ -45,21 +41,35 @@ public class MathBox extends ObjectBox<Number> {
      * метод расчитывающий сумму элементов коллекции
      */
     public void splitter (Number divisor){
-        for (int i = 1; i < this.list.size(); i++)   {
-            Double div =(double) this.list.get(i) /(double) divisor;
-            this.list.set(i,(Number) div);
+        ArrayList<Number> ms= new ArrayList<Number>();
+        ms.addAll(this.sList);
+        this.sList.clear();
+        for (int i = 1; i < ms.size(); i++)   {
+            Double div =(double) ms.get(i) /(double) divisor;
+            this.sList.add((Number) div);
         }
     }
 
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        return false;
+        MathBox mathBox = (MathBox) o;
+        return Objects.equals(sList, mathBox.sList);
     }
 
     public int hashCode() {
-        int hash = Objects.hash(list);
+        int hash = Objects.hash(this.sList);
         return hash;
+    }
+
+    @Override
+    public void dump() {
+        super.dump();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
 
