67aa651b92b9ddd6c8a4876a29bc680439dd5f1c
Dayanova <fayagul.galieva@mail.ru>
2020-05-27 21:34:26 +0300

    Classloader

--- src/com/example/part1/lesson09/task1/MyClassLoader.java
+++ src/com/example/part1/lesson09/task1/MyClassLoader.java
@@ -2,13 +2,15 @@ package com.example.part1.lesson09.task1;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class MyClassLoader extends ClassLoader {
+    private final String pathtobin = "C:\\Users\\Фаягуль\\IdeaProjects\\HelloApp\\src\\com\\example\\part1\\lesson09\\task1\\com\\example\\part1\\lesson09\\task1";
 
     @Override
     public Class<?> loadClass(String name) throws ClassNotFoundException {
-        if ("SomeClass".equals(name)) {
+        if ("SomeCLass".equals(name)) {
             return findClass(name);
         }
         return super.loadClass(name);
@@ -17,9 +19,10 @@ public class MyClassLoader extends ClassLoader {
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
         System.out.println("findClass starts work: " + name);
-        if ("SomeClass".equals(name)) {
+        Path pathToFile = Paths.get(pathtobin +"\\"+ "SomeCLass.class");
+        if (name.equals("SomeCLass")) {
             try {
-                byte[] bytes = Files.readAllBytes(Paths.get("./SomeClass.class"));
+                byte[] bytes = Files.readAllBytes(pathToFile);
                 return defineClass(name, bytes, 0, bytes.length);
             } catch (IOException e) {
                 e.printStackTrace();
@@ -27,4 +30,5 @@ public class MyClassLoader extends ClassLoader {
         }
         return super.findClass(name);
     }
+
 }
---
+++ src/com/example/part1/lesson09/task1/Worker.java
@@ -0,0 +1,6 @@
+package com.example.part1.lesson09.task1;
+
+public interface Worker {
+
+    void doWork();
+}
